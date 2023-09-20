160f33a38f8305ef40deb2ec9baa592c92d22c69
Muiota <PhilPro2006@yandex.ru>
2023-09-21 00:20:23 +0300

    export fix

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,37 +1,16 @@
 
-const COMPOSITION_PRESET_QNT = 4;
-
-
-const STORE_MAP_INSTRUMENT_TIMELINE_SIZE = 512;
-const STORE_MAP_PRESET_SIZE = 1024;
-const STORE_MAP_SONG_LIST_HEADER = 8192;
-const STORE_MAP_SONG_COUNT = 100;
-
-const SONG_PRESET_SIZE = 49152;
-
-const COMPOSITION_INSTRUMENT_QNT = 4;
-
-const COMPOSITION_TOTAL_PATTERN_LEN = 256;
-
-const GuitarPresetItem = 642;
-const CompositionPatternItem = 258;
-const DrumPatternsSetting = 12276;
-const BassPatternsSetting = 4896;
-const SynthPatternsSetting = 15048;
-const AutomationPatternsSetting = 2592;
-const LfoSettings = 36;
-const CompositionGlobalSettings = 98;
-const COMPLEX_LFO_QNT = 3;
-
 class AtacExport {
     constructor(options) {
         
         this.currentPort = undefined;
         this.iteration = 0;
+        this.lastIteration = 0;
         this.buffer = [];
         this.result = [];
-        this.globalCrc = 0;
+        this.timeoutTimer = undefined;
+        this.currentExportCrc = 0;
         this.status = {
+            isExportMode: false,
             isConnected: false,
             isAllowed: false,
             progress: 0,
@@ -55,71 +34,191 @@ class AtacExport {
         }
 
 
-        if (this.buffer.length == 34) {
-            var crc = 0;
-            this.iteration++;
+        if (this.status.isExportMode) {
+            this.updateTimeoutTimer();
+            if (this.buffer.length == 2) {
+              
+                var check = this.buffer[0] + (this.buffer[1] << 8);
+                            
+                var result = this.currentExportCrc ^ check;
+                console.log("import check ");
+                console.log(this.buffer);
+                console.log("Crc ");
+                console.log(result);
+                if (result == 17) {
+                    this.status.error = "";
+                    if (this.iteration < 1296) {
 
-            this.status.progress = Math.floor( this.iteration* 0.0771604938271605);
+                        const request = new Uint8Array(34);
 
-            for (var k = 0; k < 32; k += 2) {
-                var data = this.buffer[k] + (this.buffer[k + 1] << 8);
-                crc = crc ^ data;
-            }
+                        for (var k = 0; k < 32; k++) {
+                            request[k] = this.result[this.iteration * 32 + k];
+                        }
+                        this.status.progress = Math.floor(this.iteration * 0.0771604938271605);
 
+                        for (var k = 0; k < 32; k += 2) {
+                            var data = request[k] + (request[k + 1] << 8);
+                            crc = crc ^ data;
+                        }
+                        request[32] = crc & 255;
+                        request[33] = (crc >> 8) & 255;
+                        this.currentExportCrc = crc;
+                        const writer = this.currentPort.writable.getWriter();
+                        await writer.write(request);
+                        writer.releaseLock();
 
-            if (this.buffer[32] == ((crc >> 8) & 255) &&
-                (this.buffer[33] == (crc & 255))) {
-
-                if (this.buffer[0] == 255 &&
-                    this.buffer[1] == 255 &&
-                    this.buffer[2] == 255 &&
-                    this.buffer[3] == 255 &&
-                    this.buffer[4] == 17 &&
-                    this.buffer[5] == 0) {
-                    //console.log("header");
-                    var header = "";
-                    for (var i = 6; i < 6 + 12; i++) {
-                        header += String.fromCharCode(this.buffer[i]);
+                    } else {
+                        this.status.isExportMode = false;
                     }
 
-                    this.status.header = header;
-                    this.status.isCorrect = false;
-                    this.result = [];
-                    console.log("title '" + header + "'");
-                    this.iteration = 0;
+                } else {
+                    this.status.error = "Invalid confirm response";
+                    this.status.isExportMode = false;                   
                 }
-                else {
-                    for (var i = 0; i < 32; i++) {
-                        this.result.push(this.buffer[i]);
+
+                this.iteration++;
+
+               
+               
+                this.buffer = [];
+                this.updateStatus();
+            }
+
+        }
+        else {
+
+            if (this.buffer.length == 34) {
+                var crc = 0;
+                this.iteration++;
+
+                this.status.progress = Math.floor(this.iteration * 0.0771604938271605);
+
+                for (var k = 0; k < 32; k += 2) {
+                    var data = this.buffer[k] + (this.buffer[k + 1] << 8);
+                    crc = crc ^ data;
+                }
+
+
+                if (this.buffer[32] == ((crc >> 8) & 255) &&
+                    (this.buffer[33] == (crc & 255))) {
+                    this.status.error = "";
+                    if (this.buffer[0] == 255 &&
+                        this.buffer[1] == 255 &&
+                        this.buffer[2] == 255 &&
+                        this.buffer[3] == 255 &&
+                        this.buffer[4] == 17 &&
+                        this.buffer[5] == 0) {
+                        //console.log("header");
+                        var header = "";
+                        for (var i = 6; i < 6 + 12; i++) {
+                            header += String.fromCharCode(this.buffer[i]);
+                        }
+
+                        this.status.header = header;
+                        this.status.isCorrect = false;
+                        this.result = [];
+                        console.log("title '" + header + "'");
+                        this.iteration = 0;
+                    }
+                    else {
+                        for (var i = 0; i < 32; i++) {
+                            this.result.push(this.buffer[i]);
+                        }
+
+
+                    }
+                    console.log("ok " + this.iteration);
+                    const response = new Uint8Array(2);
+                    var result = crc ^ 17;
+                    response[0] = result & 255;
+                    response[1] = (result >> 8) & 255;
+
+
+                    const writer = this.currentPort.writable.getWriter();
+                    await writer.write(response);
+                    writer.releaseLock();
+
+                    if (this.iteration == 1296 && this.result.length == 41472) {
+                        this.status.isCorrect = true;
+                        //   this.checkCrc();
                     }
 
-                        
                 }
-                console.log("ok " + this.iteration);
-                const response = new Uint8Array(2);
-                var result = crc ^ 17;
-                response[0] = result & 255;
-                response[1] = (result >> 8) & 255;
-
-
-                const writer = this.currentPort.writable.getWriter();
-                await writer.write(response);
-                writer.releaseLock();
-
-                if (this.iteration == 1296 && this.result.length == 41472) {
-                    this.status.isCorrect = true;
-                 //   this.checkCrc();
-                }
-
+                this.buffer = [];
+                this.updateStatus();
             }
-            this.buffer = [];
-            this.updateStatus();
         }
 
 
+    }
+
+    startExport() {        
+        if (this.status.isConnected && this.status.isCorrect) {
+
+            this.startExportAsync().then((result) => { });
+        }
+    }
+
+    updateTimeoutTimer() {
+
+        if (this.timeoutTimer) {
+            clearTimeout(this.timeoutTimer);
+            this.timeoutTimer = undefined;
+        }
+        if (this.status.isExportMode) {
+            this.lastIteration = this.iteration;
+            var that = this;
+            this.timeoutTimer = setTimeout(function () {
+                if (that.lastIteration == that.iteration) {
+                    that.status.error = "Export timeout";
+                    that.status.isExportMode = false;
+                    that.updateStatus();
+                }
+            }, 5000);
+        }
+    }
+
+
+    async startExportAsync() {
+
+        if (this.status.isConnected && this.status.isCorrect) {
+
+        
+
+            const request = new Uint8Array(34);
+            request[0] = 255;
+            request[1] = 255;
+            request[2] = 255;
+            request[3] = 255;
+            request[4] = 17;
+            request[5] = 0;
+            var crc = 0;
+            for (var k = 0; k < 32; k += 2) {
+                var data = request[k] + (request[k + 1] << 8);
+                crc = crc ^ data;
+            }
+            request[32] = crc & 255;
+            request[33] = (crc >> 8) & 255;
+            this.status.isExportMode = true;
+            this.status.progress = 0;
+            this.currentExportCrc = crc;
+            this.iteration = 0;
+            
+            const writer = this.currentPort.writable.getWriter();
+            await writer.write(request);
+            writer.releaseLock();
+            this.updateTimeoutTimer();
+            this.updateStatus();
+
+
+        }
+
 
     }
-    async startExportAsync() {
+
+
+    async startListenAsync() {
+        this.status.isExportMode = false;
         this.status.isConnected = false;
         this.status.error = undefined;
         this.iteration = 0;
@@ -167,58 +266,19 @@ class AtacExport {
         this.status.isConnected = false;
         if (this.status.isAllowed) {
 
-            this.startExportAsync().then((result) => { });
+            this.startListenAsync().then((result) => { });
         }
     }
 
-    checkCrc() {
-        this.globalCrc = 0;
-
-        this.checkCrcPart(0, this.result.length);
-        this.status.isCorrect = this.globalCrc == 0;
-        return;
-
-        var seek = 0;
-        for (var item = 0; item < COMPOSITION_PRESET_QNT; item++)
-        {
-            this.checkCrcPart(seek + item * STORE_MAP_PRESET_SIZE, GuitarPresetItem);
-        }
-        seek += STORE_MAP_PRESET_SIZE * COMPOSITION_PRESET_QNT;
-
-
-        for (var item = 0; item < COMPOSITION_INSTRUMENT_QNT; item++)
-        {         
-            this.checkCrcPart(seek + item * STORE_MAP_INSTRUMENT_TIMELINE_SIZE, CompositionPatternItem);         
-        }
-        seek += STORE_MAP_INSTRUMENT_TIMELINE_SIZE * COMPOSITION_INSTRUMENT_QNT;
-
-
-        this.checkCrcPart(seek, DrumPatternsSetting);
-
-        seek += DrumPatternsSetting;
-        this.checkCrcPart(seek, BassPatternsSetting);
-        seek += BassPatternsSetting;
-
-        this.checkCrcPart(seek, SynthPatternsSetting);
-        seek += SynthPatternsSetting;
-        this.checkCrcPart(seek, AutomationPatternsSetting);
-        seek += AutomationPatternsSetting;
-
-        for (var item = 0; item < COMPLEX_LFO_QNT; item++)
-        {           
-            this.checkCrcPart(seek + item * LfoSettings, LfoSettings);
-        }
-        seek += COMPLEX_LFO_QNT * LfoSettings;
-        this.checkCrcPart(seek, CompositionGlobalSettings);
-
-        this.status.isCorrect = this.globalCrc == 0;
-    }
+    
 
     checkCrcPart(start, size) {
+        var crc = 0;
         for (var k = start; k < start + size; k += 2) {
             var data = this.result[k] + (this.result[k + 1] << 8);
-            this.globalCrc = this.globalCrc ^ data;
+            crc = crc ^ data;
         }
+        return crc;
     }
 
     updateStatus() {
@@ -247,7 +307,7 @@ var options = {
         var btnExportElement = document.getElementById("btn_export");
         var progressElement = document.getElementById("progress_status");
         var titleElement = document.getElementById("div_title");
-        
+
         console.log(status);
         btnConnectElement.disabled = !status.isAllowed || status.isConnected;
 
@@ -267,7 +327,16 @@ var options = {
 
         progressElement.style.width = status.progress + '%';
 
-        btnExportElement.disabled = !status.isConnected || !status.isCorrect;
+        btnExportElement.disabled = !status.isConnected || !status.isCorrect || status.isExportMode;
+
+
+        if (status.isExportMode) {
+            progressElement.classList.add("bg-danger");
+        } else {
+            progressElement.classList.remove("bg-danger");
+        }
+        
+
         
        
     }
